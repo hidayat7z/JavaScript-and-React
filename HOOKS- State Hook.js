c802527bcd426cfaf0ef67909f9af4b1b7736c58
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-09-24 17:12:23 +0530

    Add files via upload

---
+++ HOOKS- State Hook.js
@@ -0,0 +1,45 @@
+import React, {useState} from "react"
+
+function App() {
+    const [count, setCount] = useState(0)
+    
+    function increment() {
+        setCount(prevCount => prevCount + 1)
+    }
+    
+    function decrement() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+
+// Class version of this ::
+
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0
+//         }
+//     }
+    
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.state.count}</h1>
+//                 <button>Change!</button>
+//             </div>
+//         )
+//     }
+// }
+
+export default App
+
