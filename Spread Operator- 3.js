3a61c57d6f5600f1b967bacf87291e48626b38d7
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-10-03 02:15:30 +0530

    Add files via upload

---
+++ Spread Operator- 3.js
@@ -0,0 +1,14 @@
+
+let person = {
+    name: "Adam",
+    age: 25,
+    city: "Manchester"
+}
+
+let employee = {
+    ...person,
+    salary: 50000,
+    position: "Software Developer"
+}
+
+console.log(employee);
