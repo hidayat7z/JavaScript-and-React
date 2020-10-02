5a01b9d0768b198b74b598afaeb1d79eff1a99aa
Md Hidayat Rasool <mdhidayat03@hotmail.com>
2020-10-03 02:12:51 +0530

    Add files via upload

---
+++ Spread Operator- 1.js
@@ -0,0 +1,7 @@
+let contacts = ["Mary", "Joel", "Danny"];
+
+let personalFriends = [ ...contacts ];
+
+contacts.push("John");
+
+console.log(personalFriends);
---
+++ Spread Operator- 2.js
@@ -0,0 +1,7 @@
+let contacts = ["Mary", "Joel", "Danny"];
+
+let personalFriends = [ "David", ...contacts, "Lily" ];
+
+contacts.push("John");
+
+console.log(personalFriends);
